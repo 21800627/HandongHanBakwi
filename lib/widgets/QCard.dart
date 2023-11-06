b82c832af7e143ef34803a1584afdae104b62c42
Nayeon0615 <22000054@handong.ac.kr>
2023-11-06 18:27:48 +0900

    QCard.dart 수정

--- lib/widgets/QCard.dart
+++ lib/widgets/QCard.dart
@@ -8,7 +8,7 @@ class QCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double cardWidth = MediaQuery.of(context).size.width * 0.5;
-    double cardHeight = MediaQuery.of(context).size.height * 0.5;
+    double cardHeight = MediaQuery.of(context).size.height * 0.35;
 
     return Card(
       elevation: 0.0,
@@ -26,7 +26,7 @@ class QCard extends StatelessWidget {
             front: Container(
               width: cardWidth,
               height: cardHeight,
-              padding: EdgeInsets.all(32.0),
+              //padding: EdgeInsets.all(32.0),
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/images/Qback.png'),
@@ -35,13 +35,18 @@ class QCard extends StatelessWidget {
                 borderRadius: BorderRadius.all(Radius.circular(8.0)),
               ),
               child: Center(
-                child: Text(this.message, style: Theme.of(context).textTheme.headline6),
+                child: Text(
+                  this.message,
+                  style: Theme.of(context).textTheme.subtitle2,
+                  textAlign: TextAlign.center,
+                ),
               ),
+
             ),
             back: Container(
               width: cardWidth,
               height: cardHeight,
-              padding: EdgeInsets.all(32.0),
+              //padding: EdgeInsets.all(32.0),
               decoration: BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('assets/images/Qback.png'),
@@ -63,90 +68,3 @@ class QCard extends StatelessWidget {
 
 
 
-
-/* 기존 코드
-import 'package:flutter/material.dart';
-import 'package:flip_card/flip_card.dart';
-
-
-class QCard extends StatelessWidget {
-  String message;
-  QCard({super.key, required this.message});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 0.0,
-      color: Colors.transparent,
-      // margin: EdgeInsets.only(left: 32.0, right: 32.0, top: 20.0, bottom: 0.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          FlipCard(
-            direction: FlipDirection.HORIZONTAL,
-            side: CardSide.FRONT,
-            speed: 1000,
-            onFlipDone: (status) {
-              print(status);
-            },
-            front: Container(
-              width: MediaQuery.of(context).size.width*0.5,
-              height: MediaQuery.of(context).size.height*0.5,
-              padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 32.0, bottom: 32.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(this.message, style: Theme.of(context).textTheme.headline6),
-                  // Text('Click here to flip back',
-                  //     style: Theme.of(context).textTheme.bodyText1),
-                ],
-              ),
-            ),
-            back: Container(
-              width: MediaQuery.of(context).size.width*0.5,
-              height: MediaQuery.of(context).size.height*0.5,
-              padding: EdgeInsets.only(left: 30.0, right: 30.0, top: 32.0, bottom: 32.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 5,
-                    blurRadius: 7,
-                    offset: Offset(0, 3), // changes position of shadow
-                  ),
-                ],
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.question_answer_outlined,
-                    size: 100,
-                    color: Colors.grey,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
- */
